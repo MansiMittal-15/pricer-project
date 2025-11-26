28acbf8330eb2f40db274a780fe6ff14a310acaf
Mansi Mittal <mansimittal935@gmail.com>
2025-11-26 11:59:02 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 // app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://pricer-project.vercel.app/"],
+    origin: ["http://localhost:5173", "https://pricer-project.vercel.app"],
     credentials: true,
 };
 app.use(cors(corsOptions))
